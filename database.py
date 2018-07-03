37b826821ecdc8ff659d055635f224708eb077be
simoes <c.lucas.simoes@gmail.com>
2018-07-03 20:56:42 -0300

    merge

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 import dataset
 
-<<<<<<< HEAD
 #db = dataset.connect('sqlite:///database.db')
 db = dataset.connect('mysql://root:123@localhost/mas_db')
-=======
 # banco sqlite3 -> local
 #db = dataset.connect('sqlite:///database.db')
 
 # banco mysql
-db = dataset.connect('mysql://user:password@localhost/mydatabase')
->>>>>>> a932f43df717f1a8901b4f5fb29afa345cc7681c
+#db = dataset.connect('mysql://user:password@localhost/mydatabase')
